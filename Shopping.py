71ad7ffd96e5665e22a7ada18f2a67cedc011b36
Taavi Luik <taaviluik@gmail.com>
2020-01-09 14:17:42 +0200

    Refactoring, added visualization code

--- Shopping.py
+++ Shopping.py
@@ -13,16 +13,21 @@
 #     name: python3
 # ---
 
-# ### Shopping Optimizer, project in Advanced Algorithmics (MTAT.03.238), 2019/2020 Autumn
+# # Shopping Optimizer, project in Advanced Algorithmics (MTAT.03.238), 2019/2020 Autumn
+
+# ## Code for loading data:
 
 # +
 from collections import namedtuple
 
-# Input: path to shops file (assumes each row is a shop with X and Y coordinates, separated by whitespace),
-# path to items file (assumes each row is items for a shop, items separated by whitespace).
+# Define the Shop namedtuple outside the loading function to make it available for later use, if necessary.
+Shop = namedtuple("Shop", "id x y items")
+
+# Loads shops with items from text files.
+# Input: a) path to shops file (assumes each row is a shop with X and Y coordinates, separated by whitespace),
+# b) path to items file (assumes each row is items for a shop, items separated by whitespace).
 # Output: A collection of Shops (e.g. Shop(id=0, x=869, y=696, items=['A', 'J']))
 def load_shops(path_to_shops, path_to_items):
-    Shop = namedtuple("Shop", "id x y items")
     shops = []
     with open(path_to_shops) as file:
         for i,line in enumerate(file.readlines()):
@@ -35,15 +40,38 @@ def load_shops(path_to_shops, path_to_items):
     shops = [Shop(e[0],e[1],e[2],e[3]) for e in shops]
     return tuple(shops)
 
+
+
+# -
+
+# Load and save the shops to a variable, print them
 shops = load_shops("tsp_10.txt", "shops_10.txt")
 print("> Loaded", len(shops), "shops:")
 for shop in shops:
     print(">>",shop)
 
-    
-#Test for writing a comment
-def function():
-    print("The new fun works pew pew")
+
+# ## Visualization code
 
 # +
-#Also a test for new cell.
+from matplotlib import pyplot as plt
+import networkx as nx
+
+# Visualizes a given ordered collection of Shops using matplotlib and networkx
+def visualize(shops):
+    G = nx.DiGraph()
+    for i,shop in enumerate(shops):
+        G.add_node(shop.id, pos=(shop.x, shop.y))
+        if i<len(shops) - 1:
+            G.add_edge(i, i+1)
+    pos=nx.get_node_attributes(G,'pos')
+    lbl = {e.id:e.id for e in shops}
+    nx.draw(G, pos, labels=lbl)
+    plt.show()
+
+# Example usage:
+visualize(shops)
+
+# -
+
+
