05775fdd20934004258ff882b7ca2d351f3060e5
unknown <idamaria.orula@gmail.com>
2020-01-10 02:34:33 +0200

    initial code for the modified set cover algorithm

--- Shopping.py
+++ Shopping.py
@@ -95,6 +95,59 @@ import math
 def euclidean_distance(a, b):
     return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
 
+
 # ## Algorithm
+
+# +
+from scipy.spatial import distance
+import sys
+#input:
+    #items - list of items that need to be purchased
+    #shops - list of shops with shop ID, x,y-coordinates and list of items it has
+        #eg Shop(id=0, x=869, y=696, items=['A', 'J'])
+    #start - x and y coordinates for the starting point of the shopping trip
+    #distance matrix - how far shops are from each other
+
+#output:
+    #optimal list of shops to visit
+    
+def shopping(item_list, shop_list, start, dists): 
+    opt_shops = [] #list of shops to visit
+    purchase = item_list[:] #[e for e in item_list] #items that yet need to be purchased
+    remaining_shops = [a for a in shop_list] #shops that we haven't visited yet
+    current = start #the location we are currently at (start [x,y] or a current shop (Shop))
+    
+    while len(purchase) != 0: #some items still need to be purchased
+        
+        best = sys.maxsize #just a very large number to start comparing shops
+        best_shop = ""
+        for shop in remaining_shops: #find optimal next shop
+            #if current is list type, we must find the distance from there to a shop
+            #if current is not of list type, we are already at some shop
+            if type(current) == list: #we are not at any specific shop yet
+                dist = distance.euclidean(current, [shop.x, shop.y]) #distance from current
+            else: #to move between shops, use precalculated distances
+                dist = distances[current.id][shop.id]
+            common = len(set(shop.items).intersection(set(purchase))) #how many needed items it has
+            if common == 0: #the shop has no necessary items
+                remaining_shops.remove(shop) #so we don't need the shop
+            else:  
+                efficiency = dist / common
+                if efficiency < best: #if we found a more efficient shop, use that as comparison
+                    best = efficiency
+                    best_shop = shop
+                    
+        #update visited shops, needed items and current location
+        opt_shops.append(best_shop) #add the shop we chose to the final list
+        purchase = list(set(purchase).difference(set(best_shop.items))) #remove the bought items from list
+        remaining_shops.remove(best_shop) #no need to revisit the shop
+        #current = (best_shop.x, best_shop.y) #move to the chosen shop
+        current = best_shop
+        
+    return opt_shops
+ 
+#test it with some set of items, starting at point 0,0
+shopping(["A","E","G","H"], shops, [0,0], distances)
+# -
 
 
